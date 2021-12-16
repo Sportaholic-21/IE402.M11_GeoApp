b3ea8497d810e0734e2f2731183016acd4406f0f
Hokichi <hokichi141@gmail.com>
2021-12-16 14:23:24 +0700

    Update FE

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./layouts/client-layout/client-layout.module').then(m => m.ClientLayoutModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./layouts/auth-layout/auth-layout.module').then(m => m.AuthLayoutModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
